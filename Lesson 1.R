e632e4f934ae9b4ece36d99cc73aa5ccf2576eec
Chris <iamcalledchris@hotmail.com>
2016-01-26 10:32:18 +0100

    First version of first lesson

---
+++ Lesson 1.R
@@ -0,0 +1,23 @@
+library('car')
+
+data <- Prestige
+
+2 == 3
+2 = 3
+
+str(data)
+
+data
+
+write.table(data, 'test.txt', sep='\t')
+
+ 
+read.table('test.txt', sep = '\t') -> dataimp
+
+dataimp2 <- dataimp -> dataimp3
+
+scatterplot(prestige ~ income, data=data)
+
+help(scatterplot)
+
+?scatterplot
